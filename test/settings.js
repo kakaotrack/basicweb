dd5087c1a126af0451e63094250836d32fac1d07
김동욱 <bradl0809@naver.com>
2023-12-11 11:07:03 +0900

    init

---
+++ test/settings.js
@@ -0,0 +1,13 @@
+function Settings() {
+    this.reset = function() {
+      location.reload();
+    }
+  
+    this.popup = function() {
+      rectMode(CENTER);
+      noStroke();
+      fill('#6495ED');
+      rect(width/2, height/2, 200, 200, 5);
+    }
+  }
+  

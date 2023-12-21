ef7a846e91afff9c5c778202297a48244cccaf43
choi1090 <dnl9496@naver.com>
2023-12-21 23:31:54 +0900

    commit

--- jeongchoui/package.js
+++ jeongchoui/package.js
@@ -1,192 +1,266 @@
-//set up the class GameObject
-class GameObject {
-  constructor(x, y, type) {
-    this.x = x;
-    this.y = y;
-    this.type = type;
-  }
-}
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const message = document.getElementById('message');
+let playerLives = 3;
+let gameOver = false;
+let gameStarted = false;
+let score = 0;
+let round = 1;
 
-//this class will extend the GameObject's inherent class properties
-class Movable extends GameObject {
-  constructor(x,y, type) {
-    super(x,y, type)
-  }
+// 캔버스 크기 설정
+canvas.width = 800;
+canvas.height = 800;
 
-//this movable object can be moved on the screen
-  moveTo(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
+// 이미지 로드
+const playerImg = new Image();
+playerImg.src = 'player.png';
 
-//this is a specific class that extends the Movable class, so it can take advantage of all the properties that it inherits
-class Hero extends Movable {
-  constructor(x,y) {
-    super(x,y, 'Hero')
-  }
-}
+const enemyImg = new Image();
+enemyImg.src = 'enemyShip.png';
 
-//this class, on the other hand, only inherits the GameObject properties
-class Tree extends GameObject {
-  constructor(x,y) {
-    super(x,y, 'Tree')
-  }
-}
+const laserImg = new Image();
+laserImg.src = 'laserRed.png';
 
-//a hero can move...
-const hero = new Hero();
-hero.moveTo(5,5);
+const lifeImg = new Image();
+lifeImg.src = 'life.png';
 
-//but a tree cannot
-const tree = new Tree();
-
-//create a constant gameObject
-const gameObject = {
-  x: 0,
-  y: 0,
-  type: ''
+// 플레이어 캐릭터
+const player = {
+    x: canvas.width / 2,
+    y: canvas.height - 50,
+    width: 50,
+    height: 50,
+    speed: 5,
+    image: playerImg
 };
 
-//...and a constant movable
-const movable = {
-  moveTo(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-//then the constant movableObject is composed of the gameObject and movable constants
-const movableObject = {...gameObject, ...movable};
-
-//then create a function to create a new Hero who inherits the movableObject properties
-function createHero(x, y) {
-  return {
-    ...movableObject,
-    x,
-    y,
-    type: 'Hero'
-  }
-}
-//...and a static object that inherits only the gameObject properties
-function createStatic(x, y, type) {
-  return {
-    ...gameObject
-    x,
-    y,
-    type
-  }
-}
-//create the hero and move it
-const hero = createHero(10,10);
-hero.moveTo(5,5);
-//and create a static tree which only stands around
-const tree = createStatic(0,0, 'Tree');
-
-//set up an EventEmitter class that contains listeners
-class EventEmitter {
-  constructor() {
-    this.listeners = {};
-  }
-//when a message is received, let the listener to handle its payload
-  on(message, listener) {
-    if (!this.listeners[message]) {
-      this.listeners[message] = [];
-    }
-    this.listeners[message].push(listener);
-  }
-//when a message is sent, send it to a listener with some payload
-  emit(message, payload = null) {
-    if (this.listeners[message]) {
-      this.listeners[message].forEach(l => l(message, payload))
-    }
-  }
-}
-
-//set up a message structure
-const Messages = {
-  HERO_MOVE_LEFT: 'HERO_MOVE_LEFT'
+// 적 우주선
+const enemy = {
+  x: Math.random() * (canvas.width - 50),
+  y: 10,
+  width: 50,
+  height: 50,
+  speed: Math.random() * 2 + 1,
+  image: enemyImg
 };
-//invoke the eventEmitter you set up above
-const eventEmitter = new EventEmitter();
-//set up a hero
-const hero = createHero(0,0);
-//let the eventEmitter know to watch for messages pertaining to the hero moving left, and act on it
-eventEmitter.on(Messages.HERO_MOVE_LEFT, () => {
-  hero.move(5,0);
-});
 
-//set up the window to listen for the keyup event, specifically if the left arrow is hit, emit a message to move the hero left
-window.addEventListener('keyup', (evt) => {
-  if (evt.key === 'ArrowLeft') {
-    eventEmitter.emit(Messages.HERO_MOVE_LEFT)
-  }
-});
+// 레이저
+const lasers = [];
+const laser = {
+    width: 2,
+    height: 20,
+    speed: 8,
+    image: laserImg
+};
 
-eventEmitter.on(Messages.HERO_MOVE_LEFT, () => {
-  hero.move(5,0);
-});
+// 플레이어 라이프를 위한 life 이미지를 그리는 함수
+function drawLives() {
+    let lifeX = canvas.width - 40;
+    const lifeY = 10;
+    const lifeWidth = 30;
+    const lifeHeight = 30;
 
-
-//
-
-
-// draws a red rectangle
-//1. get the canvas reference
-canvas = document.getElementById("myCanvas");
-
-//2. set the context to 2D to draw basic shapes
-ctx = canvas.getContext("2d");
-
-//3. fill it with the color red
-ctx.fillStyle = 'red';
-
-//4. and draw a rectangle with these parameters, setting location and size
-ctx.fillRect(0,0, 200, 200) // x,y,width, height
-
-const img = new Image();
-img.src = 'path/to/my/image.png';
-img.onload = () => {
-  // image loaded and ready to be used
-}
-
-function loadAsset(path) {
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.src = path;
-    img.onload = () => {
-      // image loaded and ready to be used
-      resolve(img);
+    for (let i = 0; i < playerLives; i++) {
+        ctx.drawImage(lifeImg, lifeX, lifeY, lifeWidth, lifeHeight);
+        lifeX -= 35; // 다음 라이프를 표시할 X 위치 조정
     }
-  })
 }
 
-// use like so
-
-async function run() {
-  const heroImg = await loadAsset('assets/hero.png')
-  const monsterImg = await loadAsset('assets/monster.png')
-}
-
-async function run() {
-  const heroImg = await loadAsset('assets/hero.png')
-  const monsterImg = await loadAsset('assets/monster.png')
-
-  canvas = document.getElementById("myCanvas");
-  ctx = canvas.getContext("2d");
-  ctx.drawImage(heroImg, canvas.width/2,canvas.height/2);
-  ctx.drawImage(monsterImg, 0,0);
-}
-
-//
-
-const MONSTER_TOTAL = 5;
-const MONSTER_WIDTH = MONSTER_TOTAL * 98;
-const START_X = (canvas.width - MONSTER_WIDTH) / 2;
-const STOP_X = START_X + MONSTER_WIDTH;
-
-for (let x = START_X; x < STOP_X; x += 98) {
-    for (let y = 0; y < 50 * 5; y += 50) {
-      ctx.drawImage(enemyImg, x, y);
+// 플레이어와 적 우주선 충돌 시 라이프 감소 처리
+function handlePlayerEnemyCollision() {
+    if (player.x < enemy.x + enemy.width &&
+    player.x + player.width > enemy.x &&
+    player.y < enemy.y + enemy.height &&
+    player.y + player.height > enemy.y) {
+        playerLives--;
+        if (playerLives === 0) {
+          gameOver = true;
+        } else {
+          enemy.x = Math.random() * (canvas.width - enemy.width);
+          enemy.y = 10;
+        }
     }
-  }
+}
 
+
+// 이벤트 리스너: 플레이어 이동 및 발사
+document.addEventListener('keydown', (event) => {
+    if (!gameOver) {
+        if (event.key === 'A' || event.key === 'a') {
+            if (player.x - player.speed > 0) {
+                player.x -= player.speed;
+            }
+        } else if (event.key === 'D' || event.key === 'd') {
+            if (player.x + player.width + player.speed < canvas.width) {
+                player.x += player.speed;
+            }
+        } else if (event.key === ' ' && lasers.length < 1) {
+            fireLaser();
+        }
+    }
+});
+
+// 레이저 발사
+function fireLaser() {
+    lasers.push({
+        x: player.x + player.width / 2 - laser.width / 2,
+        y: player.y,
+        width: laser.width,
+        height: laser.height,
+        speed: laser.speed
+    });
+}
+
+// 게임 시작 메시지 표시
+function showRoundStartMessage() {
+    message.innerText = `Round ${round} Clear. New Round Starting!`;
+    message.classList.remove('hidden');
+    setTimeout(() => {
+        message.classList.add('hidden');
+    }, 2000); // 2초 후 메시지 숨김
+}
+
+// 라운드 업데이트
+function updateRound() {
+    round++;
+    showRoundStartMessage();
+    
+    // 적 우주선 위치와 플레이어 위치 부드럽게 변경 (90도 회전)
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const playerDistance = 200; // 플레이어와 적 우주선 사이의 거리
+
+    const playerAngle = ((round - 1) * Math.PI) / 2; // 라운드가 바뀔 때마다 90도(π/2) 회전
+    player.x = centerX + playerDistance * Math.cos(playerAngle);
+    player.y = centerY + playerDistance * Math.sin(playerAngle);
+
+    const enemyAngle = playerAngle + Math.PI; // 플레이어와 반대 방향으로 설정
+    enemy.x = centerX + playerDistance * Math.cos(enemyAngle);
+    enemy.y = centerY + playerDistance * Math.sin(enemyAngle);
+
+    player.imageAngle = playerAngle; // 플레이어 이미지 회전 각도 설정
+    enemy.imageAngle = enemyAngle; // 적 우주선 이미지 회전 각도 설정
+}
+
+// 게임 루프
+function draw() {
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // 플레이어 비행기 그리기
+    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
+
+    // 적 비행기 그리기
+    ctx.drawImage(enemyImg, enemy.x, enemy.y, enemy.width, enemy.height);
+
+    // 적 비행기 이동
+    if (enemy.y < canvas.height) {
+        enemy.y += enemy.speed;
+    } else {
+        enemy.x = Math.random() * (canvas.width - enemy.width);
+        enemy.y = 10;
+        enemy.speed = Math.random() * 2 + 1; // 랜덤 속도 재설정
+    }
+
+    // 레이저 그리기 및 이동
+    for (let i = 0; i < lasers.length; i++) {
+        ctx.drawImage(laserImg, lasers[i].x, lasers[i].y, lasers[i].width, lasers[i].height);
+        lasers[i].y -= lasers[i].speed;
+
+        // 적과 레이저 충돌 체크
+        if (lasers[i].x < enemy.x + enemy.width &&
+            lasers[i].x + lasers[i].width > enemy.x &&
+            lasers[i].y < enemy.y + enemy.height &&
+            lasers[i].y + lasers[i].height > enemy.y) {
+        // 적 격추
+        lasers.splice(i, 1);
+        enemy.x = Math.random() * (canvas.width - enemy.width);
+        enemy.y = 10;
+        enemy.speed = Math.random() * 2 + 1; // 랜덤 속도 재설정
+        score += 100; // 점수 추가
+        }
+        
+        // 레이저가 화면 밖으로 나가면 제거
+        if (lasers[i].y < 0) {
+            lasers.splice(i, 1);
+        }
+    }
+
+    // 플레이어와 적 충돌 체크
+    if (player.x < enemy.x + enemy.width &&
+        player.x + player.width > enemy.x &&
+        player.y < enemy.y + enemy.height &&
+        player.y + player.height > enemy.y) {
+        playerLives--;
+        if (playerLives === 0) {
+            gameOver = true;
+        } else {
+            enemy.x = Math.random() * (canvas.width - enemy.width);
+            enemy.y = 10;
+        }
+    }
+
+    // 게임 오버 텍스트 표시
+    if (gameOver) {
+        ctx.fillStyle = 'white';
+        ctx.font = '40px Arial';
+        ctx.fillText('Game Over', canvas.width / 2 - 100, canvas.height / 2);
+    }
+
+    // 점수 표시
+    ctx.fillStyle = 'white';
+    ctx.font = '24px Arial';
+    ctx.fillText('Score: ' + score, 10, 30);
+
+    // 플레이어 라이프 그리기
+    drawLives();
+
+    requestAnimationFrame(draw);
+
+    // 라운드 업데이트 조건 확인
+    if (score >= 1000 * round) {
+        updateRound();
+    }
+}
+
+// 초기화 함수
+function init() {
+    canvas.width = 800;
+    canvas.height = 800;
+    round = 1; // 라운드 초기화
+    showStartMessage();
+}
+
+// 게임 시작 함수
+function startGame() {
+    gameStarted = true;
+    message.classList.add('hidden');
+    draw();
+}
+
+// 게임 종료 함수
+function endGame() {
+    gameOver = true;
+    gameStarted = false;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    message.innerText = 'Game Over. Press Enter to Restart';
+    message.classList.remove('hidden');
+}
+
+// 이벤트 리스너: Enter 키를 눌러 게임 시작 또는 재시작
+document.addEventListener('keydown', (event) => {
+    if (!gameStarted && event.key === 'Enter') {
+        gameStarted = true;
+        message.classList.add('hidden');
+        draw();
+    } else if (gameOver && event.key === 'Enter') {
+        playerLives = 3; // 플레이어 라이프 초기화
+        gameOver = false;
+        score = 0; // 점수 초기화
+        message.classList.add('hidden');
+        draw();
+    }
+});
+
+// 초기화 함수 호출
+init();
